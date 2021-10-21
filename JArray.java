95eba9495d2ad039ed6bae5f86193980b73563cc
MohdShad123 <89112962+MohdShad123@users.noreply.github.com>
2021-10-21 18:18:46 +0530

    Add files via upload

---
+++ JArray.java
@@ -0,0 +1,19 @@
+public class JArray{
+	public static void main(String[] args) {
+		
+		int[][] array = {{10,20,30},{40,50,60}};
+
+		for(int[] i : array)
+		{
+			for(int j : i)
+			{
+			System.out.print(j +" ");
+			}
+		}
+	}
+}
+class TwoDarray{
+	public static void main(String[] args) {
+		
+	}
+}
